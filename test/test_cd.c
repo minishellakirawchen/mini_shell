b34c7d75698f19281e82c213974bc37a8d90cbc7
takira <takira@student.42tokyo.jp>
2023-01-03 21:35:41 +0900

    ADD: add cd test

--- test/test_cd.c
+++ test/test_cd.c
@@ -2,6 +2,25 @@
 
 int main(void)
 {
+	char	*current_path;
+	int		ret;
+
+	printf("### cd test ###\n");
+	current_path = getcwd(NULL, 0);
+	printf(" current:%s\n", current_path);
+
+
+	printf("cd\n");
+	ret = chdir("/bin");//absolute path
+	if (ret < 0)
+		perror("chdir");
+
+	current_path = getcwd(NULL, 0);
+	printf(" moved:%s\n", current_path);
+
+	char *cmd[5] = {"ls", NULL};
+	printf("ls\n");
+	execvp("ls", cmd);
 
 	return (0);
 }

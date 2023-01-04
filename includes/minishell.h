1ca4f01f8ed489670ac14e9620f765913c57a9db
wchen <wchen@student.42tokyo.jp>
2023-01-04 16:50:40 +0800

    upgrade structure

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,42 +6,51 @@
 /*   By: wchen <wchen@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 21:00:08 by takira            #+#    #+#             */
-/*   Updated: 2023/01/03 10:06:25 by wchen            ###   ########.fr       */
+/*   Updated: 2023/01/04 16:49:41 by wchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-# define MINISHELL_H
+#define MINISHELL_H
 
-# include <errno.h>
-# include <string.h>
+#include <errno.h>
+#include <string.h>
 
-# include <readline/readline.h>
-# include <readline/history.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-# include "./../libs/libft/libft.h"
-# include "./../libs/libftprintf/ft_printf.h"
-
+#include "./../libs/libft/libft.h"
+#include "./../libs/libftprintf/ft_printf.h"
 
 /* typedef struct */
 typedef struct s_minishell_param	t_info;
 typedef struct s_env_elem			t_env_elem;
-typedef struct s_execute_cmds		t_cmds;
-typedef enum e_
+typedef struct s_execute_cmds_info	t_cmds;
 
 /* typedef enum */
+typedef enum e_cmd_group	t_group;
 
+/* enum */
+enum e_cmd_group
+{
+	E_SHELL,
+	E_PIPE,
+	E_SUBSHELL,
+	E_AND,
+	E_OR
+};
 
 /* struct */
 struct s_env_elem
 {
-	char *key;
-	char *value;
+	char	*key;
+	char	*value;
 };
 
-struct s_execute_cmds
+struct s_execute_cmds_info
 {
-
+	t_group	type;
+	char	**cmds;
 };
 
 // builtin needed param
@@ -52,49 +61,40 @@ struct s_execute_cmds
 //	env		: env(key + = + value)
 //  exit	: exit status?
 
-
 struct s_minishell_param
 {
-	int				exit_status;
-	struct	s_list	*env_list;
-	char			**input_line; //tmp
-	char			**commands;//tmp
-
-
+	int		exit_status;
+	t_list	*env_list;
+	char	**input_line; // tmp
+	char	**commands;   // tmp
+	t_list	*execute_cmds;
 };
-
-
-/* enum */
-
 
 /* input */
 t_list	*get_env_list(void);
-int		prompt_loop(t_info	*info);
+int		prompt_loop(t_info *info);
 
 /* analysis */
-int	analysis(void); //tmp
-
+int		analysis(void); // tmp
 
 /* execution */
 int		execute(t_info *info);
 
 /* expansion */
-int		expand_variable(void); //tmp
-
+int		expand_variable(void); // tmp
 
 /* exit */
-void	free_alloc(t_info	**info);
+void	free_alloc(t_info **info);
 int		free_and_return_no(t_info **info, int exit_status);
-
 
 /* builtin */
 // return exit_status
-int	ft_echo(t_info *info);
-int	ft_cd(t_info *info);
-int	ft_pwd(t_info *info);
-int	ft_export(t_info *info);
-int	ft_unset(t_info *info);
-int	ft_env(t_info *info);
-int	ft_exit(t_info *info);
+int		ft_echo(t_info *info);
+int		ft_cd(t_info *info);
+int		ft_pwd(t_info *info);
+int		ft_export(t_info *info);
+int		ft_unset(t_info *info);
+int		ft_env(t_info *info);
+int		ft_exit(t_info *info);
 
 #endif

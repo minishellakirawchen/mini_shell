35671e43bc4338aeca9f7330e29e8f7ec243d5c4
wchen <wchen@student.42tokyo.jp>
2023-01-11 22:11:22 +0900

    commit signal test code

---
+++ playground/test_fork_signal.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_fork_signal.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wchen <wchen@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/04 18:32:45 by takira            #+#    #+#             */
+/*   Updated: 2023/01/11 22:10:08 by wchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test.h"
+#include <signal.h>
+
+void sig_parent(int signum)
+{
+	printf("sig_parent: %d\n", signum);
+}
+
+void sig_child(int signum)
+{
+	printf("sig_child: %d\n", signum);
+}
+
+int	main(void)
+{
+	/* fork cd, touch playground 22/Jan/5th */
+	//int param = 0;
+	//char *cd_cmd[10] = {"cd", NULL};
+	//char *touch_cmd[10] = {"touch", "testfile", NULL};
+	//char *ls_cmd[10] = {"ls", NULL};
+	char *cat[10] = {"cat", NULL};
+	signal(SIGQUIT, sig_parent);
+	pause();
+	int pid_t = fork();
+	if (pid_t == 0)
+	{
+		// child
+		signal(SIGQUIT, sig_child);
+		printf("child cat\n");
+		execvp(cat[0], cat);
+		printf("child after execvp\n");// not print
+		// cd -> pwd 不明
+		exit(0);
+	}
+	wait(NULL);
+	printf("parent pwd\n");
+	char *path = getcwd(NULL, 0);
+	printf("path: %s\n", path);
+
+//	int pid_t = fork();
+//	if (pid_t == 0)
+//	{
+//		// child
+//		printf("child touch\n");//childでもfile反映される
+//		execvp(touch_cmd[0], touch_cmd);
+//		printf("child after exexcp\n"); //not print it
+//		exit(0);
+//	}
+//	wait(NULL);
+//	printf("parent ls\n");
+//	execvp(ls_cmd[0], ls_cmd);
+//	printf("parent after exexcp\n"); //not print it
+
+
+
+
+
+
+
+	/* fork playground 22/Jan/4th */
+//	printf("init :%d\n", param);
+//
+//	int pid_t = fork();
+//	if (pid_t == 0)//child 1
+//	{
+//		printf("  child1 :%d\n", param);
+//		param++;
+//		printf("  child1 :%d\n", param);
+//
+//		int pid_t2 = fork();
+//		if (pid_t2 == 0)// child2
+//		{
+//			printf("    child2 :%d\n", param);
+//			param++;
+//			printf("   child2 :%d\n", param);
+//			exit(0);
+//		}
+//		wait(NULL);
+//		exit(0);
+//	}
+//	wait(NULL);
+//	printf("parent :%d\n", param);
+
+	return (0);
+}
